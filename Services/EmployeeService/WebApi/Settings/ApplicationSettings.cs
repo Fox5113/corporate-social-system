5d718c7ba5eacc2c5d0027a807a1f1e3667200ab
vmukhametova <vmukhametova@fil-it.ru>
2024-10-03 12:42:36 +0500

    #20_vmukhametova Creating EmployeeService - the service create/update/delete/show/send to rabbitmq info about employees

---
+++ Services/EmployeeService/WebApi/Settings/ApplicationSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApi.Settings
+{
+    /// <summary>
+    /// Настройки приложения
+    /// </summary>
+    public class ApplicationSettings
+    {
+        public string ConnectionString { get; set; }
+        public string SiteUrl { get; set; }
+        public string RabbitMqQueue { get; set; }
+        public string HostName { get; set; }
+
+        public ApplicationSettings() {
+            ConnectionString = "Server=localhost;Database=Employees;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true;encrypt=false;";
+            SiteUrl = "https://localhost:5202";
+            HostName = "localhost";
+            RabbitMqQueue = "MyQueue";
+        }
+    }
+}
