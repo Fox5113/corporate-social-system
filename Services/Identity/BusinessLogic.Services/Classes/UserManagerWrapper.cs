534dfb7758d4a88d19f4ed62244d7902594cd380
vmukhametova <vmukhametova@fil-it.ru>
2024-11-18 21:59:14 +0500

    vmukhametova
    Bug fix
    Поправила слияние сервиса Identity

--- Services/Identity/BusinessLogic.Services/Classes/UserManagerWrapper.cs
+++ Services/Identity/BusinessLogic.Services/Classes/UserManagerWrapper.cs
@@ -1,3 +1,4 @@
+using BusinessLogic.Models;
 using BusinessLogic.Services.Interfaces;
 using DataAccess.Entities;
 using Microsoft.AspNetCore.Identity;
@@ -19,4 +20,28 @@ public class UserManagerWrapper : IUserManagerWrapper
     public Task<IdentityResult> UpdateAsync(ApplicationUser user) => _userManager.UpdateAsync(user);
     public Task<IdentityResult> DeleteAsync(ApplicationUser user) => _userManager.DeleteAsync(user);
     public Task<bool> CheckPasswordAsync(ApplicationUser user, string password) => _userManager.CheckPasswordAsync(user, password);
+
+    public async Task<ForgotPasswordResponseDto> ForgotPassword(ForgotPasswordDto model)
+    {
+        var user = await _userManager.FindByNameAsync(model.Login);
+        var user2 = await _userManager.FindByEmailAsync(model.Email);
+        if (user == null || user?.Id != user2?.Id)//|| !(await _userManager.IsEmailConfirmedAsync(user))
+        {
+            return null;
+        }
+
+        var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+        return new ForgotPasswordResponseDto() { Id = user.Id, Code = code };
+    }
+
+    public async Task<IdentityResult> ResetPassword(ResetPasswordDto model)
+    {
+        var user = await _userManager.FindByIdAsync(model.UserId);
+        var user2 = await _userManager.FindByEmailAsync(model.Email);
+        if (user == null || user?.Id != user2?.Id)// || !(await _userManager.IsEmailConfirmedAsync(user))
+        {
+            return null;
+        }
+        return await _userManager.ResetPasswordAsync(user, model.Code, model.Password);
+    }
 }
