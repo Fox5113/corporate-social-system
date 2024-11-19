d27a6698c1c7ddf772c7b660ac9e3b5552d50142
DESKTOP-JLR2A67\Lenovo <demon5513@list.ru>
2024-11-19 03:04:35 +0300

    Небольшая шалость

--- Services/PersonalAccountV2/PersonalAccountV2/PersonalAccountV2/Startup.cs
+++ Services/PersonalAccountV2/PersonalAccountV2/PersonalAccountV2/Startup.cs
@@ -13,35 +13,26 @@ namespace PersonalAccountV2
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             InstallAutomapper(services);
             services.AddServices(Configuration);
             services.AddControllers();
             services.AddCors();
-            // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PersonalAccountV2Api", Version = "v1" });
-
             }); ;
             services.AddHostedService<RabbitMqListener>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
@@ -49,15 +40,9 @@ namespace PersonalAccountV2
 
             app.UseRouting();
             app.UseAuthorization();
-
             if (!env.IsProduction())
             {
-                // Enable middleware to serve generated Swagger as a JSON endpoint.
                 app.UseSwagger();
-
-                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-                // specifying the Swagger JSON endpoint.
-
                 app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
@@ -69,9 +54,8 @@ namespace PersonalAccountV2
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("--");
                 });
-
                 endpoints.MapControllers();
             });
         }
